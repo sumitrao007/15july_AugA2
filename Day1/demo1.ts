e6f29af5bf92f51dff6dda8a7d69ac7b0913e62b
sumitrao007 <raokhande.sumit@gmail.com>
2019-11-05 15:04:11 +0530

    typescript day1

---
+++ Day1/demo1.ts
@@ -0,0 +1,68 @@
+console.log("Welcome to My Angular Session!!!!");
+// var a;
+// var a1;
+// var a_b;
+// var a$b;
+// var ab;
+// number,string,boolean,any,void
+var a:number;
+var b:string;
+var c:boolean;
+var d;
+var a1:number=12;
+var a2:number=20;
+console.log("Value of a1 is "+a1);
+console.log(`
+---------------------------
+Value of A1 is ${a1} ${a2}
+
+`);
+
+console.log("----------------------------");
+console.log("value of A! is ---- "+a1);
+
+var str:string="Sumit Raokhande";
+var bool:boolean=true;
+console.log(`
+value of string str is ${str} boolean value is ${bool}
+`);
+var a3:any;
+var a4:void;
+a3=23;
+console.log("Value of a3 is "+a3);
+a3="Sumit raokhande";
+console.log("Value of a3 is "+a3);
+a3=false;
+console.log("Value of a3 is "+a3);
+a3=12;
+console.log("Value of a3 is "+a3);
+
+var d1:number=30;
+console.log("Type of D1 is "+typeof(d1));
+console.log("Type of D1 is "+typeof d1 );
+var d2:string="Hefshine";
+console.log("Type of D2 is "+typeof d2);
+
+var c1:number;
+console.log("value of c1 is "+c1);
+
+//type assertion
+// 1 Angle bracket
+// 2 As syntax
+
+var b1:any;
+//angle bracket syntax
+
+//(<string> b1).
+
+//as syntax
+
+// (b1 as string).
+
+
+
+
+
+
+
+
---
+++ Day1/demo2.ts
@@ -0,0 +1,113 @@
+// //var,const, let
+
+// const a:number=12;
+// console.log(`
+// ----------------
+// Value of Const a is ${a}
+// `);
+
+// // Airthmatic,
+// //logical- &&,||,==
+// //relational,
+// //ternary/conditional- 
+// //bitwise &,|,^,~
+
+// var a1:number=5;
+// var a2:number=20;
+// console.log(`
+// ------------------------
+// Addition is ${a1+a2}
+
+// `);
+
+// var temp:number=a1>a2?a1+a2:a2-a1;
+
+// console.log(`
+// ------------------------
+// Ternary Operator Addition  is ${temp}
+
+// `);
+
+// var temp2:number=a1>a2?a1>5?a1+5:a1-5:a2-a1;
+// console.log(`
+// ------------------------
+// Value  is ${temp2}
+// `);
+
+// const b1:number=12;
+// const b2:number=19;
+
+// if(b1>b2){
+//     console.log("B1 is greater " );
+// }else{
+//     console.log("B2 Is Greater");
+// }
+
+
+// if(a1>b1){
+//     if(a1>b2){
+//         console.log("A1 is greater ");
+//     }
+
+// }else if(a2>b1){
+//     if(a2>b2){
+//         console.log("A2 is greater ");
+//     }
+
+// }
+
+// var key:number=-1;
+// switch (key) {
+//     case 1:
+//         console.log("Value of Key is "+key);
+//         break;
+
+//     default:
+//             console.log("Value of Key is in Default case"+key);
+//         break;
+// }
+
+// var key1:string="hefshine";
+// switch (key1) {
+//     case "hefshine":
+//         console.log("Value of Key is "+key1);
+//         break;
+
+//     case "Sumit":
+//         console.log("Value of key is "+key1);
+//         break; 
+
+//     default:
+//             console.log("Value of Key is in Default case"+key1);
+//         break;
+// }
+
+// for(let i=0;i<5;i++){
+//     console.log("value of Count is "+i+2);
+// }
+//console.log(i);
+//const element:number=1;
+for (let i = 0; i < 5; i++) {
+    const element = i;
+    console.log("Value of element is "+element);    
+}
+
+var a:number=5;
+console.log("Value of a is "+a);
+while(a>0){
+    a--;
+}
+console.log("Value of a is "+a);
+a=10;
+do{
+a--;
+console.log("In Do While loop"+a);
+}while(a>0);
+
+
+
+
+
+
+
+
