4c257b5745fa30398239ad3e4221709d6a4b2087
sumitrao007 <raokhande.sumit@gmail.com>
2019-11-22 13:56:22 +0530

    Home Update

--- Angular/Day10/app/home/home.component.ts
+++ Angular/Day10/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -15,11 +16,33 @@ export class HomeComponent implements OnInit {
     rank:1
   };
 
-  constructor() { }
+  constructor(private route:Router) { }
 
   ngOnInit() {
     this.myid=9;
     this.myusername="Ashwini Patil";
+  }
+
+  onClick(value){
+    if(value==1){
+     // console.log("In If Loop");
+     this.myid=7;
+      this.route.navigate(['/PAGE1',this.myid]);
+
+    }else if(value==2){
+      this.myid=5;
+      this.myusername="Pranali Aug A2"
+      this.route.navigate(['/PAGE2',this.myid,this.myusername]);
+    }else if(value==3){
+      this.queryobj.brand="Google";
+      this.queryobj.year=1995;
+      this.queryobj.rank=1
+      this.route.navigate(['/QueryParamter'],
+      { queryParams: this.queryobj })
+
+    }else if(value==4){
+      this.route.navigate(['/Follower']);
+    }
   }
 
 }
